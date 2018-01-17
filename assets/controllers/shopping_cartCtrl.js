b911d0fcf7bb8eabe7bda18e98a362631e386d7d
Ankit Sharma <ankitkumarsharma939@gmail.com>
2018-01-18 01:12:56 +0530

    fdf

--- assets/controllers/shopping_cartCtrl.js
+++ assets/controllers/shopping_cartCtrl.js
@@ -1,3 +1,10 @@
 angular.module('myApp').controller('shopping_cartCtrl', ['$routeParams','$scope','RestaurantService', function($routeParams,RestaurantService){
 	$scope.items = RestaurantService.cart;
+	$scope.filter = '1';
+
+	$scope.next(flag){
+		if(flag == 0){
+			
+		}
+	}
 }])
