5be9a6479647c5733ca56939d5b5b9afcdeaa480
Ankit Sharma <ankitkumarsharma939@gmail.com>
2018-01-23 17:25:43 +0530

    asdf

--- assets/controllers/shopping_cartCtrl.js
+++ assets/controllers/shopping_cartCtrl.js
@@ -1,10 +1,23 @@
 angular.module('myApp').controller('shopping_cartCtrl', ['$routeParams','$scope','RestaurantService', function($routeParams,RestaurantService){
 	$scope.items = RestaurantService.cart;
 	$scope.filter = '1';
+	angular.forEach($scope.items, function(item){
+		$scope.total_amount += item.price*item.number;
+	})
 
-	$scope.next(flag){
+	$scope.next = function(flag){
 		if(flag == 0){
+			
+		}else if(flag == 1){
+			
+		} else if(flag == 2){
 			
 		}
 	}
+
+	$scope.$watch('items', function(oldValue, newValue){
+		angular.forEach($scope.items, function(item){
+			$scope.total_amount += item.price*item.number;
+		})
+	}, true)
 }])
